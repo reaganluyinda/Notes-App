534ef539403631bfd40322b04d66359b894a9c80
reagan luyinda <luyindareagan@gmail.com>
2026-03-13 20:02:34 +0300

    create NoteForm component

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,13 @@
+const NoteForm = ({ value, handleChange, onSubmit }) => {
+    return(
+<div>
+    <h2>Create a new note</h2>
+    <form onSubmit={onSubmit}>
+        <input value={value} onChange={handleChange}/>
+        <button type="submit">save</button>
+    </form>
+</div>
+    )
+}
+
+export default NoteForm;
