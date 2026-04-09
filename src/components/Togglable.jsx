375843d2420ecbf69fbd91ba16bcdce22d924e96
reagan luyinda <luyindareagan@gmail.com>
2026-04-09 18:34:40 +0300

    ref; remove unused comsole logs from togglable component

--- src/components/Togglable.jsx
+++ src/components/Togglable.jsx
@@ -5,8 +5,7 @@ const Togglable = forwardRef((props, ref) => {
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
-  console.log(props.children)
-
+ 
   const toggleVisibility = () => {
     setVisible(!visible)
   }
