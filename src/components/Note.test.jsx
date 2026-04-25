43f446eac01f2410c8be4cac4ea7e2360399ca9f
reagan luyinda <luyindareagan@gmail.com>
2026-04-25 14:19:36 +0300

    chore; install user-event library to simulate the testing of user inputs

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -3,12 +3,14 @@ import Note from './Note'
 
 test('renders content', () => {
   const note = {
-    content: 'Component testing is done with react-testing-library',
+    content: 'component testing is done with react-testing-library',
     important: true
   }
 
   render(<Note note={note} />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  const element = screen.getByText('component testing is done with react-testing-library')
+
+  screen.debug(element)
   expect(element).toBeDefined()
 })
